dc4a1f52bfc5304bad449d8dd1ebbab9f3704199
Papujay Kumar <papunjaykumar127@gmail.com>
2024-02-02 07:14:37 +0530

    after registration directed to login page

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import Swal from 'sweetalert2';
 
@@ -9,7 +10,7 @@ import Swal from 'sweetalert2';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent {
-  constructor(private userSerice:UserService,private snack: MatSnackBar){}
+  constructor(private userSerice:UserService,private snack: MatSnackBar,private _router:Router){}
   
   public user={
     userName:'',
@@ -32,12 +33,13 @@ export class SignupComponent {
       });
       return;
     }
-
+    
     // adduser
     this.userSerice.addUser(this.user).subscribe(
       (data:any)=>{
         // success
         console.log(data);
+        this._router.navigate(["/login"]);
         Swal.fire('Successfully done !!','User  Id is '+data.id,'success')
         
       },
